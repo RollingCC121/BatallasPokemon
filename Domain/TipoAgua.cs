84b4063ee95af0c20fcb20bbf39a994561f58416
AndresNavarrom <andresmarintuprigci@gamil.com>
2026-02-17 21:21:51 -0500

    se agrega la estructura por carpetas

---
+++ Domain/TipoAgua.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace batallasPokemon.Domain
+{
+    public class TipoAgua : ITipo
+    {
+        // DECISIÓN POR FALTA DE DETALLE EN UML: Id constante para identificar el tipo
+        public string Id => "Agua";
+    }
+}
